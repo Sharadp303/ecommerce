4d060cba8833072e29c0bb34970ad2a110581d6b
SHARAD PATEL <sharad.patels03@gmail.com>
2022-08-25 21:45:12 +0530

    lets check

--- config/server.config.js
+++ config/server.config.js
@@ -1,14 +1,14 @@
 //let serverPort = 4500;
 // require('dotenv').config()
 // if(process.env.NODE_ENV !== 'development'){
-	
+
 // 	serverPort = process.env.PORT
 // }
 
 // module.exports = {
 // 	serverPort: serverPort
 // }
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
 
